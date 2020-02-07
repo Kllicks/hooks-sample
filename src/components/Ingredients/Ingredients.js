6e15c8b38640e640c5502d96f26bc0fb1ddde58b
Kyle Sekellick <sekellickkyle@yahoo.com>
2020-02-07 17:55:49 -0500

    sending http requests with fetch and using react hooks

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+	const [ userIngredients, setUserIngredients ] = useState([]);
+
+	const addIngredientHandler = (ingredient) => {
+		fetch('https://react-hooks-sample-a514a.firebaseio.com/ingredients.json', {
+			method: 'POST',
+			body: JSON.stringify({ ingredient }),
+			headers: { 'content-Type': 'application/json' }
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((responseData) => {
+				setUserIngredients((prevIngredients) => [
+					...prevIngredients,
+					{ id: responseData.name, ...ingredient }
+				]);
+			});
+	};
+
 	return (
 		<div className="App">
-			<IngredientForm />
+			<IngredientForm onAddIngredient={addIngredientHandler} />
 
 			<section>
 				<Search />
-				{/* Need to add list here! */}
+				<IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
 			</section>
 		</div>
 	);
-}
+};
 
 export default Ingredients;
